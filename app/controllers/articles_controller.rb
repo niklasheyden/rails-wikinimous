b26eb50eca660804b42c8411af560b8e088cc3e8
Niklas von Heyden <niklas.heyden@me.com>
2020-08-11 17:53:08 +0200

    fix destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    redirect_to article_path
+    redirect_to articles_path
   end
 
   private
